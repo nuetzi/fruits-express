7dd138546c10a570c172e49455de994f5525ba05
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-08 13:51:34 -0500

    Work In Progress

---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Fruit = require('./models/fruits.js');
+
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended:true}));
+
+
+// Setup view engine above routes
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+
+// Create a page that will allow us to create a new fruit
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.redirect('/fruits');
+    });
+});
+
+
+// Index route
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('Index', {
+            fruits: allFruits
+        });
+    });
+});
+
+
+// form POST
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){       //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else {                                //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.send(createdFruit);
+    });
+});
+
+
+app.get('/fruits/:id', (req, res)=>{
+    Fruit.findById(req.params.id, (err, foundFruit)=>{
+        res.render('Show', {
+            fruit:foundFruit
+        });
+    });
+});
+
+
+
+// Connect to mongo database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('Connected to mongo');
+});
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
---
+++ views/New.jsx
@@ -0,0 +1,22 @@
+const React = require('react');
+
+class New extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>New Fruit Page</h1>
+                <form action='/fruits' method='POST'>
+                    Name: <input type='text' name='name' />
+                    <br></br>
+                    Color: <input type='text' name='color' />
+                    <br></br>
+                    Is Ready To Eat: <input type='checkbox' name='readyToEat' />
+                    <br></br>
+                    <input type='submit' name='' value='Create Fruit' />
+                </form>
+            </div>
+        )
+    }
+}
+
+module.exports = New;
