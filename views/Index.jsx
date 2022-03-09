c5603c81116b3497fd496775cafb218cd7986ad7
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 10:43:35 -0500

    Formatting adjustment on Index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,14 +9,17 @@ class Index extends React.Component {
                 {
                     this.props.fruits.map((fruit,i) => {
                         return <li key={i}>
-                            <a href={`/fruits/${fruit.id}`}>{fruit.name}</a>
-                            is { fruit.readyToEat? <span>It is ready to eat</span>: <span> It is not ready to eat </span>}
+                            The <a href={`/fruits/${fruit.id}`}>{fruit.name}</a> is {fruit.color}
+                            <br></br>
+                            { fruit.readyToEat? <span>It is ready to eat</span>: <span> It is not ready to eat </span>}
                             <br></br>
                             <a href={`/fruits/${fruit.id}/edit`}>Edit This Fruit</a>
                             {/* Your Delete Form Goes Here  It's incomplete we will fix below*/}
                             <form action={`/fruits/${fruit._id}?_method=DELETE`} method="POST">
                                 <input type="submit" value="DELETE"/>
                             </form>
+                            <br></br>
+                            <br></br>
                         </li>
                     })
                 }
