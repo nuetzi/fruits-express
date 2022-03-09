46f3dd76b46ce924bfc976d935e52e7a797f6976
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 11:24:35 -0500

    Clean up and comment code

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 
 const port = process.env.PORT || 3000;
 
-// Middleware -- Goes near the top
+// Middleware -- Executes for all routes, so it goes near the top
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
@@ -17,28 +17,51 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 
+// SEED values
+app.get('/fruits/seed', (req, res) => {
+    Fruit.create([
+        {
+            name:'grapefruit',
+            color:'pink',
+            readyToEat:true
+        },
+        {
+            name:'grape',
+            color:'purple',
+            readyToEat:false
+        },
+        {
+            name:'avocado',
+            color:'green',
+            readyToEat:false
+        }
+    ], (err, data) => {
+        res.redirect('/fruits')
+    });
+});
+
+
+// INDEX route: Shows ALL
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('Index', {fruits: allFruits});
+    });
+});
+
+
 // Create a page that will allow us to create a new fruit
+// Goes above the SHOW route
 app.get('/fruits/new', (req, res) => {
     res.render('New');
 });
 
 
-// Index route: Show ALL
-app.get('/fruits', (req, res)=>{
-    Fruit.find({}, (error, allFruits)=>{
-        res.render('Index', {
-            fruits: allFruits
-        });
-    });
-});
-
-
-// form POST
+// POST -- New fruit; CREATE route
 app.post('/fruits/', (req, res)=>{
-    if(req.body.readyToEat === 'on'){       //if checked, req.body.readyToEat is set to 'on'
-        req.body.readyToEat = true;
-    } else {                                //if not checked, req.body.readyToEat is undefined
-        req.body.readyToEat = false;
+    if(req.body.readyToEat === 'on') {      // If checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;         // Do some data correction
+    } else {                                // If not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;        // Do some data correction
     }
     Fruit.create(req.body, (error, createdFruit)=>{
         res.redirect('/fruits');
@@ -46,21 +69,27 @@ app.post('/fruits/', (req, res)=>{
 });
 
 
-app.put('/fruits/:id', (req, res)=>{
-    if(req.body.readyToEat === 'on'){
-        req.body.readyToEat = true;
-    } else {
-        req.body.readyToEat = false;
-    }
-    Fruit.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
-        res.redirect('/fruits');
+// Add a SHOW route
+app.get('/fruits/:id', (req, res) => {
+    Fruit.findById(req.params.id, (err, foundFruit) => {
+        res.render('Show', {fruit: foundFruit});
     });
 });
 
 
-app.get('/fruits/:id/edit', (req, res)=>{
-    Fruit.findById(req.params.id, (err, foundFruit)=>{          //find the fruit
-      if(!err){
+// DELETE route -- Goes beneath SHOW route
+app.delete('/fruits/:id', (req, res) => {
+    Fruit.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect('/fruits');            //redirect back to fruits index
+    });
+});
+
+
+// EDIT route - Gets the form, prepopulated, to edit the fruit
+// Goes beneath the DELETE route
+app.get('/fruits/:id/edit', (req, res) => {
+    Fruit.findById(req.params.id, (err, foundFruit) => {          //find the fruit
+      if(!err) {
         res.render(
     		  'Edit',
     		{
@@ -74,20 +103,15 @@ app.get('/fruits/:id/edit', (req, res)=>{
 });
 
 
-// GET: Show one
-app.get('/fruits/:id', (req, res) => {
-    Fruit.findById(req.params.id, (err, foundFruit) => {
-        res.render('Show', {
-            fruit:foundFruit
-        });
-    });
-});
-
-
-// DELETE: Delete one
-app.delete('/fruits/:id', (req, res) => {
-    Fruit.findByIdAndRemove(req.params.id, (err, data) => {
-        res.redirect('/fruits');            //redirect back to fruits index
+// PUT route -- Second part of the EDIT route
+app.put('/fruits/:id', (req, res)=>{
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    Fruit.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
+        res.redirect('/fruits');
     });
 });
 
