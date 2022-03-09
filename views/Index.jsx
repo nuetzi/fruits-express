be70ecbdd78caa909905f6e51b9c55fec35ead5f
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 11:50:25 -0500

    Format adjustment for Index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,7 +5,7 @@ class Index extends React.Component {
     return (
         <div>
             <link rel="stylesheet" href="/css/app.css"/>
-            <h1>Fruits index page</h1>
+            <h1>Fruits Index Page</h1>
             <ul>
                 {
                     this.props.fruits.map((fruit, i) => {
@@ -14,12 +14,12 @@ class Index extends React.Component {
                             <br/>
                             { fruit.readyToEat? <span>It is ready to eat.</span>: <span> It is not ready to eat.</span>}
                             <br/>
-                            <br/>
                             <a href={`/fruits/${fruit.id}/edit`}>Edit This Fruit</a>
                             {/* Your Delete Form Goes Here  It's incomplete we will fix below*/}
                             <form action={`/fruits/${fruit._id}?_method=DELETE`} method="POST">
                                 <input type="submit" value="DELETE"/>
                             </form>
+                            <br/>
                             <br/>
                         </li>
                     })
